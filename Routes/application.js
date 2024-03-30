649f36404e2870c73911aa42f62b506419f382b0
BahaEddinDridi <bahaeddine170@gmail.com>
2024-03-30 06:09:55 +0100

    Application CRUD

--- Routes/application.js
+++ Routes/application.js
@@ -5,4 +5,6 @@ const ApplicationController = require('../controllers/controller.application');
 // Route to handle resume upload
 router.post('/saveApplication', ApplicationController.saveApplication);
 
+router.put('/updateApplication/:applicationId', ApplicationController.updateApplication);
+
 module.exports = router;
