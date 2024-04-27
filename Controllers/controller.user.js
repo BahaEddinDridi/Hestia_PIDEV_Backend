7f43bdf4614b2a4eccc3a4731124a9a3ab5cec93
sarraTouhami <152702200+Sarra-Touhami@users.noreply.github.com>
2024-04-27 20:58:43 +0100

    pull main

--- Controllers/controller.user.js
+++ Controllers/controller.user.js
@@ -234,6 +234,7 @@ const getAllUsers = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
 module.exports = {
     registerUser,
     updateprofile,
