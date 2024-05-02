1cd47da4f6fe545ef8aad26f7cae18bdf6b433f6
Mohsen_Bernaoui <Mohsen.bernaoui@gmail.com>
2024-05-02 15:07:40 +0100

    Update CORS configuration to allow requests from 192.168.33.10:5173

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cors({
 app.use(express.static('dist'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.33.10:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
